794fae6c7d048cc1f3fb451675b4ee3d11acaf16
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-06-21 15:17:03 -0300

    primeira parte ok

---
+++ src/components/IdCard/index.js
@@ -0,0 +1,30 @@
+export function IdCard(props) {
+  return (
+    <ul>
+      <div className="pic">
+        <img src={props.picture} alt="#"></img>
+
+        <li>
+          <strong>First Name:</strong>
+          {props.firstName}
+        </li>
+        <li>
+          <strong>Last Name:</strong>
+          {props.lastName}
+        </li>
+        <li>
+          <strong>Gender:</strong>
+          {props.gender}
+        </li>
+        <li>
+          <strong>Height:</strong>
+          {props.height}
+        </li>
+        <li>
+          <strong>Birth:</strong>
+          {props.birth.toDateString()}
+        </li>
+      </div>
+    </ul>
+  );
+}
